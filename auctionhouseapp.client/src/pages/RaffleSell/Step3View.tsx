0bb9ef873c9935242f97637488080ebde93ae4da
Rely Ky <relyky@hotmail.com>
2025-09-17 14:06:03 +0800

    raffle/sell - UAT

--- auctionhouseapp.client/src/pages/RaffleSell/Step3View.tsx
+++ auctionhouseapp.client/src/pages/RaffleSell/Step3View.tsx
@@ -16,6 +16,7 @@ export default function RaffleSell_Step3View() {
   return (
     <Container maxWidth='sm'>
       <Typography variant='h5' gutterBottom>寄送抽獎券({raffleOrder?.raffleOrderNo})</Typography>
+      <Box sx={{ mb: 2 }} >抽獎券張數：{raffleOrder?.purchaseCount}</Box>
 
       {/* raffleOrder && <RaffleOrderWidget raffleOrder={raffleOrder} /> */}
 
