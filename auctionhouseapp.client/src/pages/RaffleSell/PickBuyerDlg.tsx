6b977d8a7fe275d58ea4f1461cde86314a7843a2
Rely Ky <relyky@hotmail.com>
2025-09-17 18:30:48 +0800

    tune 畫面

--- auctionhouseapp.client/src/pages/RaffleSell/PickBuyerDlg.tsx
+++ auctionhouseapp.client/src/pages/RaffleSell/PickBuyerDlg.tsx
@@ -93,9 +93,9 @@ export default function PickBuyerDlg(props: {
           <Table stickyHeader>
             <TableHead>
               <TableRow>
-                <TableCell sx={{ textWrap: 'nowrap' }}>客戶名稱</TableCell>
-                <TableCell sx={{ textWrap: 'nowrap' }}>電郵地址</TableCell>
-                <TableCell sx={{ textWrap: 'nowrap' }}>聯絡電話</TableCell>
+                <TableCell sx={{ whiteSpace: 'nowrap', textWrap: 'nowrap' }}>客戶名稱</TableCell>
+                <TableCell sx={{ whiteSpace: 'nowrap', textWrap: 'nowrap' }}>電郵地址</TableCell>
+                <TableCell sx={{ whiteSpace: 'nowrap', textWrap: 'nowrap' }}>聯絡電話</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
