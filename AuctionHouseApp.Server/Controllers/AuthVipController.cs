621ec02755851a4f4afe4af973a8d2d5307fe405
Rely Ky <relyky@hotmail.com>
2025-09-20 11:16:42 +0800

    AuthVipLogin

---
+++ AuctionHouseApp.Server/Controllers/AuthVipController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionHouseApp.Server.Controllers;
+
+[Route("api/auth/vip")]
+[ApiController]
+public class AuthVipController(
+    ILogger<AuthVipController> _logger
+  ) : ControllerBase
+{
+  [HttpPost("[action]")]
+  public ActionResult<AuthVipLoginResult> Login([FromBody] AuthVipLoginArgs args)
+  {
+    try
+    {
+      bool isValid = (args.Name == "smart" && args.Email == "smart@mail.server");
+      if (!isValid) return Ok(new AuthVipLoginResult(false, null));
+
+      // 模擬已存在的貴賓
+      string token = "sims-token";
+      var guest = new AuthVipLoginResult_Guest("001", args.Name, args.Email, "A1");
+
+      // SUCCESS
+      var result = new AuthVipLoginResult(true,
+        new AuthVipLoginResult_Data(token, guest));
+
+      _logger.LogInformation("AuthVip/Login SUCCESS. {guestName} {guestName}", guest.Id, guest.Name);
+      return Ok(result);
+    }
+    catch (Exception ex)
+    {
+      _logger.LogError(ex, "出現例外！{message}", ex.Message);
+      return Ok(new AuthVipLoginResult(false, null));
+    }
+
+
+  }
+}
---
+++ AuctionHouseApp.Server/Controllers/AuthVipDto.cs
@@ -0,0 +1,48 @@
+﻿using Reinforced.Typings.Attributes;
+
+namespace AuctionHouseApp.Server.Controllers;
+
+//json
+//{
+//  "name": "string",  // 姓名
+//  "email": "string",  // Email
+//}
+
+[TsInterface(Namespace = "dto.authVip")]
+public record AuthVipLoginArgs(string Name, string Email);
+
+
+//** Response:**
+//json
+//{
+//  "success": true,
+//  "data": {
+//    "token": "string",
+//    "guest": {
+//      "id": "string",
+//      "name": "string",
+//      "email": "string",
+//      "tableNumber": "string"
+//    }
+//  }
+//}
+
+[TsInterface(Namespace = "dto.authVip")]
+public record AuthVipLoginResult(
+  Boolean Success,
+  AuthVipLoginResult_Data? Data
+);
+
+[TsInterface(Namespace = "dto.authVip")]
+public record AuthVipLoginResult_Data(
+  string Token,
+  AuthVipLoginResult_Guest Guest
+);
+
+[TsInterface(Namespace = "dto.authVip")]
+public record AuthVipLoginResult_Guest(
+  string Id,
+  string Name,
+  string Email,
+  string TableNumber
+ );
