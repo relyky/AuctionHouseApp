3233ee31dcdda8221674159a89eb6dd6fd51f107
Rely Ky <relyky@hotmail.com>
2025-09-24 12:14:22 +0800

    DB schema -> DTO

--- auctionhouseapp.client/src/vite-env.d.ts
+++ auctionhouseapp.client/src/vite-env.d.ts
@@ -63,63 +63,74 @@ interface ILotProfile {
 /**
  * 拍品資訊
  */
-interface ILot {
-  lotNo: string
-  lotTitle: string
-  lotDesc: string
-  catalog: string
-  highEstimate: number
-  lowEstimate: number
-  reservePrice: number
-  startPrice: number
-  status: string
-}
-
-interface ILiveAuctionStatus {
-  /**
-   * 系統版次。這個欄位是用來判斷是否有更新
-   */
-  rowversion: number
-  step: StepEnum
-  curLotNo: string
-  curBidPrice: number
-  bidIncrement: number
-  isLocked: boolean
-  isBidOpen: boolean
-  isHammered: boolean
-  bidOpenSn: number
-  thisBidOpenTime?: string
-  thisBidCloseTime?: string
-  lastBiddingEventUpdDtm?: string // ISO 8601 格式的時間字串
-}
-
-interface IBidMsg {
-  lotNo: string
-  bidderNo: string
-  bidPrice: number
-  bidOpenSn: number // int
-}
-
-interface IBiddingEvent {
-  biddingSn: number
-  lotNo: string
-  bidderNo: string
-  bidPrice: number
-  isValid: 'Y' | 'N'
-  bidOpenSn: number
-  bidTimestamp: string // ISO 8601 格式的時間字串
-}
-
-interface IHammeredRecord {
-  lotNo: string
-  bidResult: string
-  winnerNo: string
-  hammerPrice: number
-  biddingSn: number
-  hammerTime: string // ISO 8601 格式的時間字串
-}
+//interface ILot {
+//  lotNo: string
+//  lotTitle: string
+//  lotDesc: string
+//  catalog: string
+//  highEstimate: number
+//  lowEstimate: number
+//  reservePrice: number
+//  startPrice: number
+//  status: string
+//}
+//
+//interface ILiveAuctionStatus {
+//  /**
+//   * 系統版次。這個欄位是用來判斷是否有更新
+//   */
+//  rowversion: number
+//  step: StepEnum
+//  curLotNo: string
+//  curBidPrice: number
+//  bidIncrement: number
+//  isLocked: boolean
+//  isBidOpen: boolean
+//  isHammered: boolean
+//  bidOpenSn: number
+//  thisBidOpenTime?: string
+//  thisBidCloseTime?: string
+//  lastBiddingEventUpdDtm?: string // ISO 8601 格式的時間字串
+//}
+//
+//interface IBidMsg {
+//  lotNo: string
+//  bidderNo: string
+//  bidPrice: number
+//  bidOpenSn: number // int
+//}
+//
+//interface IBiddingEvent {
+//  biddingSn: number
+//  lotNo: string
+//  bidderNo: string
+//  bidPrice: number
+//  isValid: 'Y' | 'N'
+//  bidOpenSn: number
+//  bidTimestamp: string // ISO 8601 格式的時間字串
+//}
+//
+//interface IHammeredRecord {
+//  lotNo: string
+//  bidResult: string
+//  winnerNo: string
+//  hammerPrice: number
+//  biddingSn: number
+//  hammerTime: string // ISO 8601 格式的時間字串
+//}
 
 //#region 與 DB Schema 對應的資料表介面定義
+interface IVip {
+  paddleNum: string;
+  vipName: string;
+  vipEmail: string;
+  vipPhone: string;
+  tableNumber: string;
+  seatNumber: string;
+  isEnterprise: string;
+  receiptHeader: string;
+  taxNum: string;
+}
 
 interface IRaffleOrder {
   raffleOrderNo: string;
@@ -146,6 +157,28 @@ interface IRaffleTicket {
   buyerPhone: string;
   emailTimes: number;
   lastEmailDtm: string;
+}
+
+interface IGiveOrder {
+  giveOrderNo: string;
+  paddleNum: string;
+  giftId: string;
+  purchaseCount: number;
+  purchaseAmount: number;
+  hasPaid: string;
+  salesId: string;
+  soldDtm: string;
+  status: string;
+  remark: string;
+  isChecked: string;
+  checker: string;
+  checkedDtm: string;
+}
+
+interface IGiveTicket {
+  giveTicketNo: string;
+  giveOrderNo: string;
+  paddleNum: string;
 }
 
 //#endregion
