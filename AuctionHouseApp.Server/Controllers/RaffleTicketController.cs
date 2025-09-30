10b22e11ded0010cdb638fe0f9c6f217371d187e
Rely Ky <relyky@hotmail.com>
2025-09-30 18:22:33 +0800

    5.3 取得我的票券

--- AuctionHouseApp.Server/Controllers/RaffleTicketController.cs
+++ AuctionHouseApp.Server/Controllers/RaffleTicketController.cs
@@ -1,4 +1,5 @@
-﻿using Dapper;
+﻿using AuctionHouseApp.Server.Services;
+using Dapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -10,7 +11,8 @@ namespace AuctionHouseApp.Server.Controllers;
 [Route("api/[controller]")]
 [ApiController]
 public class RaffleTicketController(
-    ILogger<RaffleTicketController> _logger
+    ILogger<RaffleTicketController> _logger,
+    AuthVipService _vipSvc
 ) : ControllerBase
 {
   /// <summary>
@@ -144,4 +146,73 @@ WHERE PrizeId = @PrizeId
       return Ok(new CommonResult<dynamic>(false, null, errMsg));
     }
   }
+
+  /// <summary>
+  /// 5.3 取得我的票券
+  /// **GET** `api/raffleticket/mytickets`
+  /// </summary>
+  /// <returns>
+  /// **Headers:**
+  /// - `Authorization: Bearer {token}`
+  /// ** Response:**
+  /// ```json
+  /// {
+  ///   "success": true,
+  ///   "data": {
+  ///     "tickets": [
+  ///       {
+  ///         "ticketNumber": "string", //票券ID
+  ///         "purchaseTime": "ISO 8601", //購買時間
+  ///         "isWinner": boolean, //是否中獎
+  ///         "prizeId": "string" | null, //中獎獎品ID
+  ///         "prizeName": "string" | null //中獎獎品名稱
+  ///       }
+  ///     ],
+  ///     "totalCount": number
+  ///   }
+  /// }
+  /// ```
+  /// </returns>
+  [Authorize(AuthenticationSchemes = "Bearer")]
+  [HttpGet("[action]")]
+  public async Task<ActionResult<CommonResult<dynamic>>> MyTickets()
+  {
+    string paddleNum = _vipSvc.PaddleNum;
+
+    try
+    {
+      // 查詢所有商品預覽 (整合 VIP 和員工資訊)
+      string sql = """
+SELECT T.RaffleTicketNo, O.RaffleOrderNo, O.SoldDtm, V.PaddleNum
+ FROM [RaffleTicket] T (NOLOCK)
+ INNER JOIN RaffleOrder O (NOLOCK) ON T.RaffleSoldNo = O.RaffleOrderNo
+ INNER JOIN [Vip] V (NOLOCK) ON T.BuyerEmail = V.VipEmail
+ WHERE V.PaddleNum = @PaddleNum
+""";
+
+      using var conn = await DBHelper.AUCDB.OpenAsync();
+      var infoList = await conn.QueryAsync(sql, new { PaddleNum = paddleNum });
+      var ticketList = infoList.Select(row => new
+      {
+        RaffleTicketNo = row.RaffleTicketNo, // 抽獎券編號
+        RaffleOrderNo = row.RaffleOrderNo,   // 訂單編號
+        SoldDtm = row.SoldDtm,
+        PaddleNum = row.PaddleNum, // 貴賓編號
+      }).ToArray();
+
+      var result = new CommonResult<dynamic>(
+          true,
+          new { Tickets = ticketList },
+          null);
+
+      return Ok(result);
+    }
+    catch (Exception ex)
+    {
+      string errMsg = string.Format("Exception！{0}", ex.Message);
+      _logger.LogError(ex, errMsg);
+      return Ok(new CommonResult<dynamic>(false, null, errMsg));
+    }
+  }
+
 }
