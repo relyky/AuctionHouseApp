eed3e1f74fcbb12960694772278f28857b125033
Rely Ky <relyky@hotmail.com>
2025-10-03 11:56:29 +0800

    更新 schema

--- Vista.DB/Schema/RaffleWinner.cs
+++ Vista.DB/Schema/RaffleWinner.cs
@@ -14,17 +14,25 @@ public class RaffleWinner
   /// 獎品ID
   /// </summary>
   [Display(Name = "獎品ID")]
+  [Key]
   public string PrizeId { get; set; } = default!;
   /// <summary>
   /// 中獎抽獎券編號
   /// </summary>
   [Display(Name = "中獎抽獎券編號")]
+  [Key]
   public string RaffleTickerNo { get; set; } = default!;
+  /// <summary>
+  /// 抽獎時間
+  /// </summary>
+  [Display(Name = "抽獎時間")]
+  public DateTime? DrawDtm { get; set; }
 
   public void Copy(RaffleWinner src)
   {
     this.PrizeId = src.PrizeId;
     this.RaffleTickerNo = src.RaffleTickerNo;
+    this.DrawDtm = src.DrawDtm;
   }
 
   public RaffleWinner Clone()
@@ -32,6 +40,7 @@ public class RaffleWinner
     return new RaffleWinner {
       PrizeId = this.PrizeId,
       RaffleTickerNo = this.RaffleTickerNo,
+      DrawDtm = this.DrawDtm,
     };
   }
 }
